848795527786dbcbe4cfc2df10421bec6e79b4f2
Yang Cheng <annacheng09@hotmail.com>
2022-04-26 22:19:00 -0700

    work on routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,20 +27,6 @@ module.exports = {
             return res.status(500).json(err);
           });
       },
-    // update user
-    updateUser(req, res) {
-        User.findOneAndUpdate(
-          { _id: req.params.userId },
-          { $set: req.body },
-          { runValidators: true, new: true }
-        )
-          .then((user) =>
-            !user
-              ? res.status(404).json({ message: 'No user with this id!' })
-              : res.json(user)
-          )
-          .catch((err) => res.status(500).json(err));
-      },
     // delete user
     deleteUser(req, res) {
         User.findOneAndUpdate({ _id: req.params.userId })
